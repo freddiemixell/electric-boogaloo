17fdc258b0b0339647c914fdf20b883408de72bc
freddiemixell <fmixell@gmail.com>
2019-11-02 14:17:23 -0500

    adding layout to interior pages

--- frontend/pages/page.js
+++ frontend/pages/page.js
@@ -1,5 +1,11 @@
+import Layout from '../components/layouts/Layout';
+
 const Page = () => {
-    return <div>Hello from interior pages.</div>
+    return (
+        <Layout>
+            <h1>Hello from interior pages.</h1>
+        </Layout>
+    )
 }
 
 export default Page;
