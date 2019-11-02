656d4159e2474e8ac3ca65d2686274c8eab7a65b
freddiemixell <fmixell@gmail.com>
2019-11-02 10:24:19 -0500

    declarative routing

--- frontend/Router.js
+++ frontend/Router.js
@@ -3,19 +3,35 @@ import {
     Route,
     Switch,
 } from 'react-router-dom';
+import { Query } from 'react-apollo';
 
 import Index from './pages/index';
+import Page from './pages/page';
 
-const AwesomeRouter = () => {
-    return (
-        <Router>
-            <Switch>
-              <Route exact path="/" component={Index} />
-              <Route path="*" component={Index} />
-            </Switch>
-        </Router>
-    );
-  };
+import { ROUTE_QUERY } from './store/Queries';
 
-  export default AwesomeRouter;
+const AwesomeRouter = () => (
+  <Query query={ROUTE_QUERY}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+        return data.pages.edges.map(({ node: { slug } }) => {
+          return (
+            <Router>
+                <Switch>
+                  <Route exact path="/" component={Index} />
+                  <Route path={`/${ slug }`} component={Page} />
+                  <Route path="*" component={Index} />
+                </Switch>
+            </Router>
+          );
+        }) 
+    }}
+  </Query>
+);
+
+export default AwesomeRouter;
+
+
   
