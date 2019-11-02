5661d726eed2e0a79814f58272abe424f731fcb0
freddiemixell <fmixell@gmail.com>
2019-11-02 14:17:53 -0500

    code spliting routes

--- frontend/Router.js
+++ frontend/Router.js
@@ -1,3 +1,4 @@
+const { Suspense, lazy } = React;
 import {
     BrowserRouter as Router,
     Route,
@@ -6,27 +7,31 @@ import {
 import { Query } from 'react-apollo';
 
 import Index from './pages/index';
-import Page from './pages/page';
+const Page = lazy(() => import('./pages/page'));
 
 import { ROUTE_QUERY } from './store/Queries';
+
+const Loading = () => <div>Loading...</div>;
 
 const AwesomeRouter = () => (
   <Query query={ROUTE_QUERY}>
     {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
+      if (loading) return <Loading/>;
       if (error) return <p>Error :(</p>;
-
-        return data.pages.edges.map(({ node: { slug } }) => {
-          return (
+        return (
+          
             <Router>
-                <Switch>
-                  <Route exact path="/" component={Index} />
-                  <Route path={`/${ slug }`} component={Page} />
-                  <Route path="*" component={Index} />
-                </Switch>
+                  <Switch>
+                    <Route exact path="/" component={Index} />
+                    <Suspense fallback={<Loading/>}>
+                      {
+                        data.pages.edges.map(({ node: { slug } }) => <Route key={slug} path={`/${ slug }`} render={() => <Page/>} />)
+                      }
+                    </Suspense>
+                    <Route path="*" component={Index} />
+                  </Switch>
             </Router>
-          );
-        }) 
+        )
     }}
   </Query>
 );
